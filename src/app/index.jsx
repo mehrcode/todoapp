898fbe16d4ec38ed418b0442f0b7b796418a4142
mehrnoosh <mehrnoosh.shoolestani@gmail.com>
2024-09-11 10:32:42 +0330

    working on the app

---
+++ src/app/index.jsx
@@ -0,0 +1,14 @@
+import { store } from './store/index';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Main } from './components/Main';
+// import { Dashboard } from './components/Dashboard';
+
+
+console.log(store.getState());
+
+ReactDOM.render(
+    <Main />,
+    document.getElementById("app")
+);
+
